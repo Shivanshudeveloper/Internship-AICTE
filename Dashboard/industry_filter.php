8a3e785d307a3288c2d85ab563c908b772ba0f3a
green4450 <t.green7244@student.sbccd.edu>
2020-03-22 21:26:34 +0530

    commited today

--- Dashboard/industry_filter.php
+++ Dashboard/industry_filter.php
@@ -99,8 +99,6 @@ $res = mysqli_query($conn,$sql);
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
-
-<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
 <div class="container">
 	<div class="row">
 		<div class="col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
